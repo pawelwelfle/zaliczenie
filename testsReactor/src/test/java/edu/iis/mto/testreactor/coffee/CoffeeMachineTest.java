b2a94b6b1c42de117301f3aef96d0d27d6a22a5e
pawelwelfle <pawel.welfle@gmail.com>
2020-06-08 18:39:58 +0200

    Failure

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -1,6 +1,10 @@
 package edu.iis.mto.testreactor.coffee;
 
+import static edu.iis.mto.testreactor.coffee.CoffeType.ESPRESSO;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 import org.hamcrest.Matchers;
@@ -8,7 +12,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.awt.*;
 
 @ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
@@ -20,12 +27,36 @@ class CoffeeMachineTest {
     private CoffeeReceipes coffeeReceipes;
     @Mock
     private MilkProvider milkProvider;
+    private CoffeOrder order;
 
     @BeforeEach
     void setUp() throws Exception {
         coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
     }
 
+    @Test
+    void checkIfCoffeCanBeCreate() {
+        Coffee coffee = new Coffee;
+        CoffeeReceipe receipe = coffeeReceipes.getReceipe(ESPRESSO);
+        grinder.grind(CoffeeSize.SMALL);
+
+         CoffeOrder espressoCoffeOrder = CoffeOrder.builder()
+                .withSize(CoffeeSize.SMALL)
+                .withType(ESPRESSO)
+                .build();
+
+        assertEquals(espressoCoffeOrder, receipe);
+    }
+
+    @Test
+    void checkIfCoffeCanBeDone(){
+        coffeeMachine.make(order.builder()
+        .withSize(CoffeeSize.SMALL)
+        .withType(CoffeType.ESPRESSO)
+        .build());
+
+        verify(coffeeMachine).make();
+    }
 
     @Test
     public void itCompiles() {
